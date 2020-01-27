ae408282e17d4404189e84c876edc9c9869a94ed
adam cushner <adam.cushner@xaxis.com>
2020-01-27 10:46:50 -0500

    added docstrings

--- pyaoc2019/06.py
+++ pyaoc2019/06.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-import asyncio
+from asyncio import run, create_task, gather
 from itertools import count
 from operator import itemgetter
 
@@ -13,6 +13,7 @@ __author__ = 'acushner'
 
 
 def parse_file(name):
+    """return {parent: {children}} map and {child: parent} map"""
     res_pc, res_cp = defaultdict(set), {}
     for l in U.read_file(name):
         parent, child = l.split(')')
@@ -22,11 +23,21 @@ def parse_file(name):
 
 
 async def calc_orbits(orbital_map, n_parents=0, current_body='COM'):
-    tasks = (asyncio.create_task(calc_orbits(orbital_map, n_parents + 1, p)) for p in orbital_map[current_body])
-    return n_parents + sum(await asyncio.gather(*tasks))
+    """
+    each body directly orbits around one body and indirectly around all that body's parents
+
+    starting from the center of the universe, sum up all of these in a recursive fashion
+    """
+    tasks = (create_task(calc_orbits(orbital_map, n_parents + 1, p)) for p in orbital_map[current_body])
+    return n_parents + sum(await gather(*tasks))
 
 
 def _get_parents(orbital_map, name):
+    """
+    for a given orbital body, all of its parents/ancestors and its distance to each
+
+    return {parent_name: distance}
+    """
     res, c = {}, count()
     while name := orbital_map.get(name):
         res[name] = next(c)
@@ -34,6 +45,7 @@ def _get_parents(orbital_map, name):
 
 
 def calc_num_transfers(orbital_map):
+    """find all parents, find the closest one based on distance, and then calculate the total to travel between them"""
     santa_fam, my_fam = _get_parents(orbital_map, 'SAN'), _get_parents(orbital_map, 'YOU')
     shared_ancestors = set(santa_fam) & my_fam.keys()
     closest_parent, s_dist = min(((k, santa_fam[k]) for k in shared_ancestors), key=itemgetter(1))
@@ -43,7 +55,7 @@ def calc_num_transfers(orbital_map):
 def __main():
     with U.localtimer():
         parent_child_map, child_parent_map = parse_file('06')
-        print(asyncio.run(calc_orbits(parent_child_map)))
+        print(run(calc_orbits(parent_child_map)))
         print(calc_num_transfers(child_parent_map))
 
 
