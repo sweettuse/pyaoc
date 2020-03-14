5e2df66d0be50726a4b0702bd5d87c9da4220c3a
acushner <sweettuse@gmail.com>
2020-03-14 17:04:29 -0400

    finished 3, mid-18

--- pyaoc2017/aoc03.py
+++ pyaoc2017/aoc03.py
@@ -1,20 +1,91 @@
 __author__ = 'acushner'
 
-from itertools import count
+from pyaoc2019.utils import Coord
 
-data = 312051
+from itertools import count, product
+
+from cytoolz.itertoolz import first
+
+location = 312051
 
 
-def get_closest_odd_square(data):
-    for i in count(1, 2):
-        if i ** 2 > data:
-            return i - 1
+def get_square_side(data):
+    return first(i for i in count(1, 2) if i ** 2 >= data)
+
+
+def aoc03_a(location):
+    """only works where the location doesn't 'wrap around' from the bottom of the square
+
+    e.g., based on this input:
+
+    17  16  15  14  13
+    18   5   4   3  12
+    19   6   1   2  11
+    20   7   8   9  10
+    21  22  23---> ...
+
+    [21, 25], [7, 9], and [1] would work, but 17, e.g, would not
+    """
+    side = get_square_side(location)
+    end = side ** 2
+    ec = side // 2
+    end_coord = Coord(ec, ec)
+    end_coord -= Coord(end - location, 0)
+    return end_coord.manhattan
+
+
+def _gen_coords():
+    """
+    create coords in the order they would appear in the data structure in the docstring in aoc03_a
+    coords:
+    0, 0
+
+    1, 0
+    1, 1
+    0, 1
+    -1, 1
+    -1, 0
+    -1, -1,
+    0, -1
+    1, -1
+
+    """
+    for i, size in zip(count(1), count(3, 2)):
+        cur = Coord(i, 1 - i)  # the start of the next outer square (bottom right corner)
+        cur_offset = Coord(0, 1)
+        offset_dict = {
+            Coord(i, i): Coord(-1, 0),
+            Coord(-i, i): Coord(0, -1),
+            Coord(-i, -i): Coord(1, 0),
+        }
+        yield cur
+        n_elements = size ** 2 - (size - 2) ** 2
+        for _ in range(n_elements - 1):
+            cur += cur_offset
+            yield cur
+            cur_offset = offset_dict.get(cur, cur_offset)
+
+
+def aoc03_b(value):
+    """generate square with neighbors to figure out first > value occurrence
+
+    represent square as dict of {coord: value}
+    """
+    offsets = {Coord(x, y) for x, y in product(range(-1, 2), range(-1, 2))}
+    grid = {Coord(0, 0): 1}
+
+    def _get_neighbor_vals(_c):
+        return sum(grid.get(_c + o, 0) for o in offsets)
+
+    for c in _gen_coords():
+        v = grid[c] = _get_neighbor_vals(c)
+        if v > value:
+            return v
 
 
 def __main():
-    closest = get_closest_odd_square(data)
-    perim_start = closest ** 2 + 1
-    print(closest, perim_start, data - perim_start)
+    print(aoc03_a(location))
+    print(aoc03_b(location))
 
 
 if __name__ == '__main__':
--- pyaoc2017/aoc18.py
+++ pyaoc2017/aoc18.py
@@ -1,11 +1,14 @@
-from collections import defaultdict
-from queue import Queue
-from typing import NamedTuple, Tuple, List
-
-from cytoolz.itertoolz import first, last
+import asyncio
+from asyncio import Queue, QueueEmpty
+from collections import defaultdict, Counter
 from contextlib import suppress
+from typing import NamedTuple, List
 
+from cytoolz.itertoolz import last
+import uvloop
 import pyaoc2019.utils as U
+
+uvloop.install()
 
 __author__ = 'acushner'
 
@@ -24,16 +27,22 @@ class Inst(NamedTuple):
 
 
 class Result(Exception):
-    pass
+    """stores result of calculation"""
 
 
 class Registers:
-    def __init__(self, instructions):
+    def __init__(self, name, instructions, in_q: Queue, out_q: Queue, starting_vals):
+        self.in_q = in_q
+        self.out_q = out_q
+        self._name = name
         self._insts = instructions
+        self._starting_vals = starting_vals
+
         self._regs = defaultdict(int)
-        self._sounds = []
-        self._recovered_sounds = []
         self._pc = 0
+        self._max_pc = 0
+        self._inst_counts = Counter()
+        self._send_count = 0
 
     def __setitem__(self, key, value):
         self._regs[key] = value
@@ -44,63 +53,101 @@ class Registers:
         return item
 
     @classmethod
-    def from_strs(cls, strs: List[str]):
-        return cls([Inst.from_str(s) for s in strs])
+    def from_strs(cls, name, strs: List[str], in_q, out_q, starts):
+        return cls(name, [Inst.from_str(s) for s in strs], in_q, out_q, starts)
 
-    def run(self):
-        while True:
+    async def _run_inst(self, inst: Inst):
+        await getattr(self, inst.inst)(inst.reg, *map(self.__getitem__, inst.args))
+
+    async def run(self):
+        for v in self._starting_vals:
+            await self.snd(v)
+        # while True:
+        for i in range(1000000):
             inst = self._insts[self._pc]
-            getattr(self, inst.inst)(inst.reg, *map(self.__getitem__, inst.args))
+            self._inst_counts[self._pc] += 1
+            if not i % 10000:
+                print(i, self._name, f'{self._pc}/{self._max_pc}', inst, self.in_q.qsize(),
+                      self._inst_counts.most_common(6), self._regs)
+            await self._run_inst(inst)
             self._pc += 1
+            self._max_pc = max(self._pc, self._max_pc)
 
-    def snd(self, reg):
-        self._sounds.append(self[reg])
-
-    def set(self, reg, val: int):
+    async def set(self, reg, val: int):
         self[reg] = val
 
-    def add(self, reg, val):
+    async def add(self, reg, val):
         self[reg] += val
 
-    def mul(self, reg, val):
+    async def mul(self, reg, val):
         self[reg] *= val
 
-    def mod(self, reg, val):
+    async def mod(self, reg, val):
         self[reg] %= val
 
-    def rcv(self, reg):
+    async def jgz(self, reg, val):
+        try:
+            reg_val = int(reg)
+        except Exception:
+            reg_val = self[reg]
+        if reg_val > 0:
+            self._pc += val - 1
+
+    async def snd(self, reg):
+        """send"""
+        self._send_count += 1
+        await self.out_q.put(self[reg])
+
+    async def rcv(self, reg):
+        """receive"""
+        v = await asyncio.wait_for(self.in_q.get(), 3)
+        self[reg] = v
+
+
+class RegA(Registers):
+    def __init__(self, name, instructions, in_q: Queue, out_q: Queue, starting_vals):
+        super().__init__(name, instructions, in_q, out_q, starting_vals)
+        self._sounds = []
+        self._recovered_sounds = []
+
+    @classmethod
+    def from_strs(cls, name, strs: List[str], in_q=Queue(), out_q=Queue(), starts=[]):
+        return super().from_strs(name, strs, in_q, out_q, starts)
+
+    async def snd(self, reg):
+        self._sounds.append(self[reg])
+
+    async def rcv(self, reg):
         if self[reg]:
             self._recovered_sounds.append(last(self._sounds))
             raise Result(last(self._recovered_sounds))
 
-    def jgz(self, reg, val):
-        if self[reg] > 0:
-            self._pc += val - 1
 
-
-class Reg2(Registers):
-    def __init__(self, instructions, in_q: Queue, out_q: Queue):
-        super().__init__(instructions)
-        self.in_q = in_q
-        self.out_q = out_q
-
-    def snd(self, reg):
-        self.out_q.put(self[reg])
-
-    def rcv(self, reg):
-        self[reg] = self.in_q.get()
-
-
-def aoc18_a():
-    regs = Registers.from_strs(U.read_file(18, 2017))
+async def aoc18_a():
+    insts = U.read_file(18, 2017)
+    regs = RegA.from_strs('regA', insts)
     try:
-        regs.run()
+        await regs.run()
     except Result as r:
         return r.args[0]
 
 
+async def aoc18_b():
+    fn = 18
+    q01 = Queue(), Queue()
+    r0 = Registers.from_strs('reg0', U.read_file(fn, 2017), *q01, [1, 2, 0])
+    r1 = Registers.from_strs('reg1', U.read_file(fn, 2017), *reversed(q01), [1, 2, 1])
+    try:
+        await asyncio.gather(r0.run(), r1.run())
+    except asyncio.TimeoutError:
+        print('we are done')
+    print(r0._send_count, r0._regs)
+    print(r1._send_count, r1._regs)
+
+
 def __main():
-    print(aoc18_a())
+    with U.localtimer():
+        print(asyncio.run(aoc18_a()))
 
 
 if __name__ == '__main__':
