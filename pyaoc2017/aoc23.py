5021cd629b3751bc26ed20563fb20f85e3b86993
acushner <sweettuse@gmail.com>
2020-03-19 14:39:36 -0400

    23 work

--- pyaoc2017/aoc23.py
+++ pyaoc2017/aoc23.py
@@ -2,6 +2,8 @@ from asyncio import Queue
 import asyncio
 from collections import Counter
 from contextlib import suppress
+from itertools import count
+from operator import itemgetter
 from typing import NamedTuple, List
 
 from cytoolz.itertoolz import first
@@ -29,19 +31,15 @@ class Result(Exception):
 
 
 class Registers:
-    def __init__(self, name, instructions, in_q: Queue, out_q: Queue, starting_vals=None, mode=0):
-        self.in_q = in_q
-        self.out_q = out_q
-        self._name = name
+    def __init__(self, instructions, mode=0):
         self._insts = instructions
-        self._starting_vals = starting_vals or []
+        U.exhaust(map(print, enumerate(instructions)))
 
         self._regs = dict.fromkeys('abcdefgh', 0)
         self._regs['a'] = mode
         self._pc = 0
         self._max_pc = 0
         self._inst_counts = Counter()
-        self._send_count = 0
         self._mul_count = 0
 
     def __setitem__(self, key, value):
@@ -53,22 +51,31 @@ class Registers:
         return item
 
     @classmethod
-    def from_strs(cls, name, strs: List[str], in_q, out_q, starts, mode=0):
-        return cls(name, [Inst.from_str(s) for s in strs], in_q, out_q, starts, mode)
+    def from_strs(cls, strs: List[str], mode=0):
+        return cls([Inst.from_str(s) for s in strs], mode)
 
     async def _run_inst(self, inst: Inst):
         await getattr(self, inst.inst)(inst.reg, *map(self.__getitem__, inst.args))
 
+    @property
+    def debug_str(self):
+        insts = {(iid, i_count): self._insts[iid] for iid, i_count in self._inst_counts.most_common(4)}
+        return f'{self._pc}/{self._max_pc} | {insts} | {self._regs}'
+
     async def run(self):
-        for i in range(int(1e8)):
-            inst = self._insts[self._pc]
-            self._inst_counts[self._pc] += 1
-            if not i % 1e6:
-                print(i, self._name, f'{self._pc}/{self._max_pc}', inst, self.in_q.qsize(),
-                      self._inst_counts.most_common(6), self._regs)
-            await self._run_inst(inst)
-            self._pc += 1
-            self._max_pc = max(self._pc, self._max_pc)
+        try:
+            for i in count():
+                inst = self._insts[self._pc]
+                self._inst_counts[self._pc] += 1
+                if not i % 1e6:
+                    print(i, self.debug_str)
+                    print(self._inst_counts)
+                    print()
+                self._max_pc = max(self._pc, self._max_pc)
+                await self._run_inst(inst)
+                self._pc += 1
+        finally:
+            print(self._inst_counts)
 
     async def set(self, reg, val: int):
         if reg == 'h':
@@ -88,33 +95,16 @@ class Registers:
         except ValueError:
             reg_val = self[reg]
         if reg_val:
+            # before = self._pc
             self._pc += val - 1
+            # print(f'jnz: {before} {val} {self._pc + 1}')
 
 
-def in_python(a):
-    b = c = d = e = f = g = h = 0
-
-    def sub_1():
-        nonlocal a, b, c, d, e, f, g, h
-        b *= 100
-        b -= -100000
-        c = b
-        c -= -17000
-        f = 1
-        d = 2
-        e = 2
-        g = d
-        g *= e
-
-    b = 93
-    c = b
-    if a:
-        pass
 
 
 @U.timer
 def aoc23(mode):
-    reg = Registers.from_strs('tuse', U.read_file(23, 2017), Queue(), Queue(), None, mode)
+    reg = Registers.from_strs(U.read_file(23, 2017), mode)
     try:
         asyncio.run(reg.run())
     except Exception:
@@ -122,8 +112,8 @@ def aoc23(mode):
 
 
 def __main():
-    print(aoc23(0))
-    # print(aoc23(1))
+    # print(aoc23(0))
+    print(aoc23(1))
 
 
 if __name__ == '__main__':
