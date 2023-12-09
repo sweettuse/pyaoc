eee36d188381b1a1c5f722a3146d0785fde542af
acushner <sweettuse@gmail.com>
2023-12-09 01:20:52 -0500

    day09 lol

---
+++ pyaoc2023/day09.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from itertools import cycle, pairwise
+from typing import Iterable
+
+from pyaoc2019.utils import read_file
+
+
+def _get_data(fname):
+    return [[int(v) for v in line.split()] for line in read_file(fname)]
+
+
+def _get_diffs(ints: list[int]) -> Iterable[list[int]]:
+    while any(v for v in ints):
+        yield ints
+        ints = [v2 - v1 for v1, v2 in pairwise(ints)]
+
+
+def part1(fname: str) -> int:
+    return sum(
+        diff[-1]
+        for row_diffs in map(_get_diffs, _get_data(fname))
+        for diff in row_diffs
+    )
+
+
+def part2(fname: str) -> int:
+    return sum(
+        sign * diff[0]
+        for row_diffs in map(_get_diffs, _get_data(fname))
+        for sign, diff in zip(cycle((1, -1)), row_diffs)
+    )
+
+
+if __name__ == "__main__":
+    print(part1("09.txt"))
+    print(part2("09.txt"))
